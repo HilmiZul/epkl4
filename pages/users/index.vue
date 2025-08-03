eefb233d5fa2a9aeaa1526c8e6be9a9304712f75
hilmizul <netspytux@gmail.com>
2025-08-03 19:03:35 +0700

    add new page: users/

---
+++ pages/users/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="card shadow-lg">
+    <div class="card-header">
+      <span class="h4 romana">Users</span>
+    </div>
+    <div class="card-body">
+      tes
+    </div>
+  </div>
+</template>
+
+<script setup>
+definePageMeta({
+  middleware: 'admin-auth'
+})
+</script>
