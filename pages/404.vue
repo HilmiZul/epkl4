a92895bce9d0fe5772e6bf714d6bccce2dc4dc61
hilmizul <netspytux@gmail.com>
2025-08-03 19:03:19 +0700

    sample page 404 (content only)

---
+++ pages/404.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="card py-5">
+    <div class="card-body text-center text-muted romana">
+      <h2>404</h2>
+      <h3>Halaman yang kamu tuju tidak ada.</h3>
+    </div>
+  </div>
+</template>
