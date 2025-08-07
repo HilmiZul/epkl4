aa446afc2162830998db276b6a4df4aa03b018a8
hilmizul <netspytux@gmail.com>
2025-08-07 19:04:59 +0700

    new package and update into config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,8 +3,10 @@ export default defineNuxtConfig({
   devtools: { enabled: false },
   css: [
     "bootstrap/dist/css/bootstrap.min.css",
-    "bootstrap-icons/font/bootstrap-icons.min.css"
+    "bootstrap-icons/font/bootstrap-icons.min.css",
+    "vue-multiselect/dist/vue-multiselect.min.css"
   ],
+  plugins: ['~/plugins/vue-multiselect.ts'],
   ssr: false,
   app: {
     head: {
