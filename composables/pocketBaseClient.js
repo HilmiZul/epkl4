44c3ad329d29d27861fa673d970ccff89ed6a60d
hilmizul <netspytux@gmail.com>
2025-08-26 14:42:45 +0700

    bring back port

--- composables/pocketBaseClient.js
+++ composables/pocketBaseClient.js
@@ -2,7 +2,8 @@ import PocketBase from 'pocketbase';
 
 const config = useRuntimeConfig()
 const host = config.public.apiBaseUrl
-const pb = new PocketBase(host);
+const port = config.public.apiPort
+const pb = new PocketBase(host+":"+port);
 
 export const usePocketBaseClient = () => {
   return pb
