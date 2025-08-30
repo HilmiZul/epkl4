694a2f3975832ab3be8719f884a162f1e142066b
hilmizul <netspytux@gmail.com>
2025-08-30 08:59:14 +0700

    decrease marbin bottom to fit when place

--- components/LoadingPlaceholder.vue
+++ components/LoadingPlaceholder.vue
@@ -1,5 +1,5 @@
 <template>
-  <p v-for="(r,i) in props.row" :key="i" class="placeholder-glow">
+  <p v-for="(r,i) in props.row" :key="i" class="placeholder-glow mb-2">
     <span :class="`placeholder col-${props.col} py-3`"></span>
   </p>
 </template>
