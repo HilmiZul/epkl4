9c0b0c076de1264209a596d2ba1dcd5940255be0
hilmizul <netspytux@gmail.com>
2025-10-10 21:18:03 +0700

    custom border from lib

--- components/RingkasanNotifJurnal.vue
+++ components/RingkasanNotifJurnal.vue
@@ -7,7 +7,7 @@
     </div>
     <div v-else-if="!isLoading && count > 0" class="alert alert-info">
       <i class="bi bi-info-circle"></i> Ada {{ count }} Jurnal yang belum di validasi.
-      <nuxt-link to="/jurnal" class="btn btn-warning btn-sm">Ayo validasi <i class="bi bi-arrow-right"></i></nuxt-link>
+      <nuxt-link to="/jurnal" class="btn btn-warning btn-sm border border-2 border-dark">Ayo validasi <i class="bi bi-arrow-right"></i></nuxt-link>
     </div>
   </div>
 </template>
