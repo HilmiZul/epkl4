f947f18c0d5301006eb4b9343437ef105461a11c
hilmizul <netspytux@gmail.com>
2025-10-22 23:57:23 +0700

    add new page: nilai PKL

---
+++ pages/nilai/[id].vue
@@ -0,0 +1,192 @@
+<template>
+  <div class="card">
+    <div class="card-header">
+      <loading-placeholder v-if="isLoading" row="1" col="6" />
+      <span v-else class="h4 fw-bold text-muted">Verifikasi Nilai / <span class="text-dark">{{ certificate.expand.siswa.nama }}</span></span>
+      <!-- <span v-if="!isLoading" class="float-end">
+        <nuxt-link v-if="!isCertificate" to="/nilai/tambah" class="btn btn-info btn-sm"><i class="bi bi-plus-lg"></i> Buat Baru</nuxt-link>
+      </span> -->
+    </div>
+    <div class="card-body">
+      <div v-if="isLoading" class="row py-3">
+        <div class="col-lg-12">
+          <loading-placeholder row="1" col="12" />
+        </div>
+      </div>
+      <div v-else>
+        <div v-if="isCertificate" class="row">
+          <div class="col-lg-12">
+            <form @submit.prevent="updateNilai">
+              <div class="row">
+                <div class="col-lg-6">
+                  <div class="mb-4">
+                    <label for="el_1">Nilai Elemen 1</label>
+                    <input v-model="form.nilai_elemen1" type="number" min="0" max="100" id="el_1" class="form form-control" required>
+                  </div>
+                  <div class="mb-4">
+                    <label for="el_2">Nilai Elemen 2</label>
+                    <input v-model="form.nilai_elemen2" type="number" min="0" max="100" id="el_2" class="form form-control" required>
+                  </div>
+                  <div class="mb-4">
+                    <label for="el_3">Nilai Elemen 3</label>
+                    <input v-model="form.nilai_elemen3" type="number" min="0" max="100" id="el_3" class="form form-control" required>
+                  </div>
+                  <div class="mb-4">
+                    <label for="el_4">Nilai Elemen 4</label>
+                    <input v-model="form.nilai_elemen4" type="number" min="0" max="100" id="el_4" class="form form-control" required>
+                  </div>
+                  <div class="mb-4">
+                    <div class="badge bg-info fw-bold">Total: {{ form.nilai_elemen1 + form.nilai_elemen2 + form.nilai_elemen3 + form.nilai_elemen4 }}</div>
+                  </div>
+                  <hr>
+                  <!-- <div class="mb-4 form-check form-switch">
+                    <input v-model="form.isEntrust" :checked="form.isEntrust" class="form-check-input" type="checkbox" id="entrust" switch>
+                    <label for="entrust">Buatkan Sertifikat di Sekolah?</label>
+                  </div> -->
+                  <div v-if="form.isEntrust">
+                    <div class="mb-4">
+                      <label for="pj_penandatangan">Pejabat Penandatangan</label>
+                      <input v-model="form.pj_penandatangan" type="text" id="pj_penandatangan" class="form form-control" placeholder="Contoh: CEO, Direktur, Kepala Dinas..." required>
+                    </div>
+                    <div class="mb-4">
+                      <label for="logo_iduka">Logo IDUKA</label>
+                      <div v-if="tempLogoImg" class="my-2"><img :src="`${host}/api/files/${certificate.collectionId}/${certificate.id}/${tempLogoImg}`" alt="Foto jurnal nilai" width="70"></div>
+                      <input @change="compressFileLogo" class="form form-control" type="file" id="logo_iduka" accept="image/*" />
+                    </div>
+                  </div>
+                  <div class="mb-4 form-check form-switch">
+                    <input v-model="form.isVerify" :checked="form.isVerify" class="form-check-input" type="checkbox" id="entrust" switch>
+                    <label for="entrust">Tandai valid</label>
+                  </div>
+                  <button :disabled="isSending" class="btn btn-success me-2 border border-2 border-dark mb-4">
+                    <span v-if="isSending">Sedang menyimpan</span>
+                    <span v-else>Simpan</span>
+                  </button>
+                  <nuxt-link to="/nilai" class="btn btn-danger border border-2 border-dark mb-4">Kembali</nuxt-link>
+                  <span v-if="isSaved" class="ms-2 mb-4 small fst-italic text-muted fw-bold">Berhasil diperbaharui!</span>
+                </div>
+                <div class="col-lg-6">
+                  <div class="mb-4">
+                    <div class="fw-bold pb-2"><i class="bi bi-image-fill"></i> Pratinjau Nilai Elemen</div>
+                    <img :src="`${host}/api/files/${certificate.collectionId}/${certificate.id}/${tempNilaiImg}`" alt="Foto jurnal nilai" width="100%" class="border border-2 border-dark mb-2">
+                  </div>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import Compressor from 'compressorjs'
+definePageMeta({ middleware: 'auth' })
+useHead({ title: "Nilai — e-PKL / SMKN 4 Tasikmalaya." })
+let user = usePocketBaseUser()
+let client = usePocketBaseClient()
+let config = useRuntimeConfig()
+let host = config.public.apiBaseUrl+":"+config.public.apiPort
+let route = useRoute()
+let isLoading = ref(true)
+let isSending = ref(false)
+let isSaved = ref(false)
+let isError = ref(false)
+let isErrorCompressOrExt = ref(false)
+let isCertificate = ref(false)
+let isLoadingMetaPemetaan = ref(true)
+let prokel = user.user.value.program_keahlian
+let siswa_id = user.user.value.siswa
+let certificate = ref()
+let tempLogoImg = ref()
+let tempNilaiImg = ref()
+let form = ref({
+  "nilai_elemen1": 0,
+  "nilai_elemen2": 0,
+  "nilai_elemen3": 0,
+  "nilai_elemen4": 0,
+  "logo": "",
+  "isVerify": ""
+})
+
+async function updateNilai() {
+  isSending.value = true
+  isSaved.value = false
+  try {
+    let res = await client.collection('nilai').update(route.params.id, form.value)
+    if(res) {
+      isSending.value = false
+      isSaved.value = true
+      navigateTo('/nilai')
+    }
+  } catch(err) {
+    isSending.value = false
+    isSaved.value = false
+    isError.value = true
+  }
+}
+
+async function getNilai(loading=true, isCert=false) {
+  isLoading.value = loading
+  isCertificate.value = isCert
+  client.autoCancellation(false)
+  try {
+    let res = await client.collection('nilai').getOne(route.params.id, {
+      expand: "iduka, siswa"
+    })
+    if(res) {
+      certificate.value = res
+      form.value = res
+      tempLogoImg.value = certificate.value.logo
+      tempNilaiImg.value = certificate.value.foto_jurnal_nilai
+      isLoading.value = false
+      isCertificate.value = true
+    }
+  } catch(error) {
+    isLoading.value = false
+    isCertificate.value = false
+  }
+}
+
+function compressFile(e) {
+  isErrorCompressOrExt.value = false
+  let file = e.target.files[0]
+  if(!file) return;
+  new Compressor(file, {
+    quality: 0.6,
+    success(result) {
+      form.value.foto_jurnal_nilai = result
+    },
+    error(err) {
+      console.error(err.message)
+      isErrorCompressOrExt.value = true
+    }
+  })
+}
+
+function compressFileLogo(e) {
+  let file = e.target.files[0]
+  if(!file) return
+  new Compressor(file, {
+    quality: 0.6,
+    success(result) {
+      form.value.logo = result
+    },
+    error(err) {
+      console.log(err.message)
+    }
+  })
+}
+
+onMounted(() => {
+  getNilai()
+  client.autoCancellation(false)
+  client.collection('nilai').subscribe('*', function(e) {
+    // getNilai params:
+    // - isLoading nilai default na true
+    // - isCert nilai default na false
+    if(e.action == 'update') getNilai(false, true)
+  },{})
+})
+</script>
---
+++ pages/nilai/index.vue
@@ -0,0 +1,134 @@
+<template>
+  <div class="card">
+    <div class="card-header">
+      <span class="h4 quicksand fw-bold"><i class="bi bi-patch-check"></i> Nilai</span>
+    </div>
+    <div class="card-body small">
+      <loading-placeholder v-if="isLoading" row="1" col="12" />
+      <div v-else-if="nilaiNotVerify?.length > 0" class="alert alert-warning fs-6">
+        <i class="bi bi-info-circle"></i>
+        Ada {{ nilaiNotVerify.length }} nilai yang belum di verifikasi
+      </div>
+      <div class="row">
+        <div class="col-lg-6">
+          <form @submit.prevent="getNilai">
+            <div class="my-3 mt-0 input-group">
+              <input v-model="keyword" type="search" class="form form-control form-control-lg" placeholder="🔎 Cari nama peserta" />
+              <button class="btn btn-info ms-2 border border-2 border-dark">Cari</button>
+            </div>
+          </form>
+        </div>
+        <div class="col align-content-center">
+          <LoadingPlaceholder v-if="isLoading" col="12" row="1" />
+          <div v-if="nilai" class="mb-3 text-grey float-end">{{ nilai.length }} nilai peserta</div>
+        </div>
+      </div>
+      <div class="table-responsive">
+        <table class="table table-striped table-hover">
+          <thead>
+            <tr>
+              <th width="3%" rowspan="2" class="align-content-center">#</th>
+              <th width="50%" rowspan="2" class="align-content-center">Peserta</th>
+              <th width="10%" rowspan="2" class="align-content-center">Kelas</th>
+              <th width="10%" rowspan="2" class="align-content-center">Titip Serti.</th>
+              <th width="10%" rowspan="2" class="align-content-center">Verify</th>
+              <!-- <th colspan="4">Nilai</th> -->
+            </tr>
+            <!-- <tr>
+              <th width="6%">1</th>
+              <th width="6%">2</th>
+              <th width="6%">3</th>
+              <th width="6%">4</th>
+            </tr> -->
+          </thead>
+          <tbody>
+            <tr v-if="isLoading">
+              <td colspan="4">
+                <loading-placeholder row="1" col="12" />
+                <loading-placeholder row="1" col="12" />
+                <loading-placeholder row="1" col="12" />
+                <loading-placeholder row="1" col="12" />
+                <loading-placeholder row="1" col="12" />
+              </td>
+            </tr>
+            <tr v-else-if="nilai?.length < 1">
+              <td colspan="5" class="text-center">
+                <div class="fs-1"><i class="bi bi-database-fill"></i></div>
+                <div class="pb-3">Data tidak ditemukan</div>
+              </td>
+            </tr>
+            <tr v-else v-for="(n, i) in nilai" :key="i">
+              <td>{{ i+1 }}.</td>
+              <td>
+                <nuxt-link :to="`/nilai/${n.id}`" class="link fw-bold">{{ n.expand.siswa.nama }}</nuxt-link>
+              </td>
+              <td>{{ n.expand.siswa.kelas }}</td>
+              <td>
+                <span v-if="n.isEntrust"><i class="bi bi-check-circle-fill text-success"></i></span>
+                <span v-else><i class="bi bi-x-circle-fill text-danger"></i></span>
+              </td>
+              <td>
+                <span v-if="n.isVerify"><i class="bi bi-check-circle-fill text-success"></i></span>
+                <span v-else><i class="bi bi-x-circle-fill text-danger"></i></span>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+definePageMeta({ middleware: 'auth' })
+useHead({ title: "Sertifikat — e-PKL / SMKN 4 Tasikmalaya." })
+let client = usePocketBaseClient()
+let user = usePocketBaseUser()
+let user_id = user?.user.value.id
+let role = user?.user.value.role
+let prokel = user?.user.value.program_keahlian
+let nilai = ref()
+let isLoaded = ref(false)
+let isLoading = ref(true)
+let keyword = ref()
+let nilaiNotVerify = ref('')
+if(role == 'tu') navigateTo('/404')
+
+async function getNilai(loading=true) {
+  isLoading.value = loading
+  let filter = ''
+  if(keyword.value) filter = ` && siswa.nama~"${keyword.value}"`
+  client.autoCancellation(false)
+  let res = await client.collection('nilai').getFullList({
+    filter: `iduka.pembimbing_sekolah="${user_id}" ${filter}`,
+    expand: "siswa, iduka, program_keahlian",
+    sort: "isVerify, siswa",
+  })
+  if(res) {
+    nilai.value = res
+    isLoading.value = false
+  }
+}
+
+async function getNilaiByNotVerify() {
+  isLoading.value = true
+  client.autoCancellation(false)
+  let res = await client.collection('nilai').getFullList({
+    filter: `iduka.pembimbing_sekolah="${user_id}" && isVerify=false`
+  })
+  if(res) {
+    nilaiNotVerify.value = res
+    isLoading.value = false
+  }
+}
+
+onMounted(() => {
+  getNilai()
+  getNilaiByNotVerify()
+  client.autoCancellation(false)
+  client.collection('nilai').subscribe('*', function(e) {
+    // param default getNilai: loading=true
+    if(e.action == 'create' || e.action == 'update') getNilai(false)
+  },{})
+})
+</script>
