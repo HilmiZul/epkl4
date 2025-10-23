c9ad4437f65626c24f8f4d5c64a43b60d0045b2e
hilmizul <netspytux@gmail.com>
2025-10-23 07:48:07 +0700

    rename kata yang tidak baku

--- components/RingkasanNotifJurnal.vue
+++ components/RingkasanNotifJurnal.vue
@@ -6,7 +6,7 @@
       </p>
     </div>
     <div v-else-if="!isLoading && count > 0" class="alert alert-info">
-      <i class="bi bi-info-circle"></i> Ada {{ count }} Jurnal yang belum di validasi.
+      <i class="bi bi-info-circle"></i> Ada {{ count }} Jurnal yang belum divalidasi.
       <nuxt-link to="/jurnal" class="btn btn-warning btn-sm border border-2 border-dark">Ayo validasi <i class="bi bi-arrow-right"></i></nuxt-link>
     </div>
   </div>
