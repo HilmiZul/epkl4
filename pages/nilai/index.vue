92305e04da552bbd20bc03271be9c30a70941c64
hilmizul <netspytux@gmail.com>
2025-11-02 21:26:44 +0700

    improve UI

--- pages/nilai/index.vue
+++ pages/nilai/index.vue
@@ -17,65 +17,59 @@
             </div>
           </form>
         </div>
-        <div class="col align-content-center">
+        <div class="col-lg-6 align-content-center text-end">
           <LoadingPlaceholder v-if="isLoading" col="12" row="1" />
-          <div v-if="nilai" class="mb-3 text-grey float-end badge">{{ nilai.length }} nilai peserta</div>
+          <div v-if="nilai" class="mb-3 text-dark badge me-2">{{ nilai.length }} peserta</div>
+          <div v-if="count_entrust" class="mb-3 text-white badge bg-dark">{{ count_entrust.length }} menitip sertifikat</div>
         </div>
       </div>
-      <div class="table-responsive">
-        <table class="table table-striped table-hover">
-          <thead>
-            <tr>
-              <th width="3%" rowspan="2" class="align-content-center">#</th>
-              <th width="50%" rowspan="2" class="align-content-center">Peserta</th>
-              <th width="10%" rowspan="2" class="align-content-center">Skor</th>
-              <th width="10%" rowspan="2" class="align-content-center">Sertifikat</th>
-              <th width="5%" rowspan="2" class="align-content-center">Valid</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-if="isLoading">
-              <td colspan="5">
-                <loading-placeholder row="1" col="12" />
-                <loading-placeholder row="1" col="12" />
-                <loading-placeholder row="1" col="12" />
-                <loading-placeholder row="1" col="12" />
-                <loading-placeholder row="1" col="12" />
-              </td>
-            </tr>
-            <tr v-else-if="keyword.length > 0 && nilai?.length < 1">
-              <td colspan="5" class="text-center">
-                <div class="fs-1"><i class="bi bi-database-fill"></i></div>
-                <div class="pb-3">Tidak ditemukan</div>
-              </td>
-            </tr>
-            <tr v-else-if="keyword.length < 1 && nilai?.length < 1">
-              <td colspan="5" class="text-center">
-                <div class="fs-1"><i class="bi bi-database-fill"></i></div>
-                <div class="pb-3">Belum ada nilai</div>
-              </td>
-            </tr>
-            <tr v-else v-for="(n, i) in nilai" :key="i">
-              <td>
-                <span v-if="n.isValid" class="badge text-dark">{{ i+1 }}</span>
-                <span v-else class="badge bg-danger">{{ i+1 }}</span>
-              </td>
-              <td>
-                <nuxt-link :to="`/nilai/${n.id}`" class="link fw-bold">{{ n.expand.siswa.nama }}</nuxt-link> <br>
-                <span class="text-muted small">{{ n.expand.siswa.kelas }}</span>
-              </td>
-              <td>{{ n.nilai_elemen1 + n.nilai_elemen2 + n.nilai_elemen3 + n.nilai_elemen4 }}</td>
-              <td>
-                <span v-if="n.isEntrust" class="badge bg-info">Ya</span>
-                <span v-else class="badge bg-success">Tidak</span>
-              </td>
-              <td>
-                <span v-if="n.isValid"><i class="bi bi-check-circle-fill text-success"></i></span>
-                <span v-else><i class="bi bi-x-circle-fill text-danger"></i></span>
-              </td>
-            </tr>
-          </tbody>
-        </table>
+      <div class="row">
+        <div class="col-lg-12">
+          <div class="table-responsive">
+            <table class="table table-hover">
+              <tbody>
+                <tr v-if="isLoading">
+                  <td colspan="5">
+                    <loading-placeholder row="1" col="12" />
+                    <loading-placeholder row="1" col="12" />
+                    <loading-placeholder row="1" col="12" />
+                    <loading-placeholder row="1" col="12" />
+                    <loading-placeholder row="1" col="12" />
+                  </td>
+                </tr>
+                <tr v-else-if="keyword.length > 0 && nilai?.length < 1">
+                  <td colspan="5" class="text-center">
+                    <div class="fs-1"><i class="bi bi-database-fill"></i></div>
+                    <div class="pb-3">Tidak ditemukan</div>
+                  </td>
+                </tr>
+                <tr v-else-if="keyword.length < 1 && nilai?.length < 1">
+                  <td colspan="5" class="text-center">
+                    <div class="fs-1"><i class="bi bi-database-fill"></i></div>
+                    <div class="pb-3">Belum ada nilai</div>
+                  </td>
+                </tr>
+                <tr v-else v-for="(n, i) in nilai" :key="i" class="border-bottom border-2 border-dark">
+                  <td width="3%">
+                    <span v-if="n.isValid" class="badge text-dark">{{ i+1 }}</span>
+                    <nuxt-link v-else :to="`/nilai/${n.id}`"><span class="badge bg-danger">{{ i+1 }}</span></nuxt-link>
+                  </td>
+                  <td>
+                    <nuxt-link :to="`/nilai/${n.id}`" class="link fw-bold">{{ n.expand.siswa.nama }}</nuxt-link> <br>
+                    <div class="text-muted fw-bold mt-2">{{ n.expand.siswa.kelas }}</div>
+                    <div v-if="n.isEntrust" class="text-muted">
+                      <i class="bi bi-person-vcard-fill"></i> Titip Sertifikat
+                    </div>
+                    <div class="text-muted">
+                      <span v-if="n.isValid"><i class="bi bi-check-circle-fill"></i> Valid</span>
+                      <nuxt-link v-else :to="`/nilai/${n.id}`" class="link-non-underscore"><span class="text-danger"><i class="bi bi-x-circle-fill"></i> Belum validasi</span></nuxt-link>
+                    </div>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -94,6 +88,7 @@ let isLoaded = ref(false)
 let isLoading = ref(true)
 let keyword = ref('')
 let nilaiNotValid = ref('')
+let count_entrust = ref('')
 if(role == 'tu') navigateTo('/404')
 
 async function getNilai(loading=true) {
@@ -124,8 +119,21 @@ async function getNilaiByNotVerify() {
   }
 }
 
+async function getEntrust() {
+  isLoading.value = true
+  client.autoCancellation(false)
+  let res = await client.collection('nilai').getFullList({
+    filter: `iduka.pembimbing_sekolah="${user_id}" && isEntrust=true`,
+  })
+  if(res) {
+    count_entrust.value = res
+    isLoading.value = false
+  }
+}
+
 onMounted(() => {
   getNilai()
+  getEntrust()
   getNilaiByNotVerify()
   client.autoCancellation(false)
   client.collection('nilai').subscribe('*', function(e) {
