86d438ec82a97e544e6abf18549eca70f828c397
hilmizul <netspytux@gmail.com>
2025-11-10 21:06:07 +0700

    initial page

---
+++ pages/rapor/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="card">
+    <div class="card-header">
+      <span class="h4 quicksand fw-bold"><i class="bi bi-book"></i> Rapor</span>
+      <!-- <button class="btn btn-info btn-sm border border-2 border-dark float-end">
+        <i class="bi bi-download"></i> Unduh
+      </button> -->
+    </div>
+    <div class="card-body">
+      <div class="alert alert-info">
+        <ul class="mb-0">
+          <li>Pilih nama peserta untuk melihat pratinjau Rapor.</li>
+          <li>Apabila sudah benar, klik tombol <span class="fw-bold">Unduh</span> diatas.</li>
+        </ul>
+      </div>
+      <h1 class="pt-5 text-center text-muted">Belum tersedia</h1>
+      <p class="pb-5 text-center text-muted">Format template sedang dibuat</p>
+    </div>
+  </div>
+</template>
