0f1bccd19824e764a4a8aa77767e055b639d756a
hilmizul <netspytux@gmail.com>
2025-11-11 10:13:24 +0700

    add componenet for wakasek &|| tu

--- pages/index.vue
+++ pages/index.vue
@@ -35,14 +35,14 @@
       </div>
     </div>
 
-    <!-- ROLE: TU -->
+    <!-- ROLE: TU & WAKASEK -->
     <div v-else class="card">
       <div class="card-header">
-        <span class="h4 quicksand fw-bold"><i class="bi bi-pie-chart-fill"></i> Ringkasan</span>
+        <span class="h4 quicksand fw-bold"><i class="bi bi-pie-chart-fill"></i> Overview</span>
       </div>
       <div class="card-body">
         <div class="row">
-          <div class="col-md-3">
+          <div class="col-md-6">
             <nuxt-link to="/pemetaan/pkl" class="link border-0">
               <div class="card">
                 <div class="card-body bg-info">
@@ -52,12 +52,26 @@
                       <span class="placeholder col-6"></span>
                     </p>
                   </h4>
-                  <span class="medium">Pemetaan</span>
+                  <span class="medium">Semua Pemetaan</span>
                 </div>
               </div>
             </nuxt-link>
           </div>
+          <div class="col-md-6">
+            <div class="card">
+              <div class="card-body bg-success">
+                <h2 v-if="!isLoadingPeserta" class="fs-bold">{{ jumlah_peserta }}</h2>
+                <h4 v-else>
+                  <p class="placeholder-glow">
+                    <span class="placeholder col-6"></span>
+                  </p>
+                </h4>
+                <span class="medium">Semua Peserta</span>
+              </div>
+            </div>
+          </div>
         </div>
+        <ringkasan-stat-per-jurusan />
       </div>
     </div>
   </div>
@@ -77,7 +91,9 @@ let user = usePocketBaseUser()
 let client = usePocketBaseClient()
 let role = user?.user.value.role
 let isLoading = ref(true)
+let isLoadingPeserta = ref(true)
 let jumlah_pemetaan = ref([])
+let jumlah_peserta = ref(0)
 
 async function getInfo() {
   isLoading.value = true
@@ -89,7 +105,18 @@ async function getInfo() {
   }
 }
 
+async function getPeserta() {
+  isLoadingPeserta.value = true
+  client.autoCancellation(false)
+  let res_siswa = await client.collection('siswa').getList(1,1)
+  if(res_siswa) {
+    jumlah_peserta.value = res_siswa.totalItems
+    isLoadingPeserta.value = false
+  }
+}
+
 onMounted(() => {
   getInfo()
+  getPeserta()
 })
 </script>
