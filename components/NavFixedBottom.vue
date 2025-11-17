3678503eeee0561380f15d4fb6e0ea64b0aa7d7e
hilmizul <netspytux@gmail.com>
2025-11-17 16:41:21 +0700

    improve navfixed bottom

--- components/NavFixedBottom.vue
+++ components/NavFixedBottom.vue
@@ -1,32 +1,32 @@
 <template>
-  <nav class="navbar navbar-expand fixed-bottom bg-white border-top border-3 border-dark p-0">
-    <div class="container-fluid justify-content-around">
+  <nav class="navbar navbar-expand fixed-bottom bg-white border-top border-3 border-dark pt-1">
+    <div class="container-fluid justify-content-center">
       <ul class="navbar-nav text-center fw-bold">
-        <li class="nav-item me-3">
+        <li class="nav-item me-4">
           <nuxt-link class="nav-link text-dark" to="/">
             <i class="bi bi-house-door fs-2"></i> <br>
             <!-- <span class="smallest">Home</span> -->
           </nuxt-link>
         </li>
-        <li class="nav-item me-3">
+        <li class="nav-item">
           <nuxt-link class="nav-link text-dark" to="/pemetaan/pkl">
             <i class="bi bi-diagram-3-fill fs-2"></i> <br>
             <!-- <span class="smallest">Pemetaan</span> -->
           </nuxt-link>
         </li>
-        <li class="nav-item me-3">
+        <li class="nav-item mx-4">
           <nuxt-link class="nav-link  text-dark" to="/jurnal">
             <i class="bi bi-journals fs-2"></i> <br>
             <!-- <span class="smallest">Jurnal</span> -->
           </nuxt-link>
         </li>
-        <li class="nav-item me-2">
+        <li class="nav-item">
           <nuxt-link class="nav-link text-dark" href="/leger">
             <i class="bi bi-patch-check fs-2"></i> <br>
             <!-- <span class="smallest">Nilai</span> -->
           </nuxt-link>
         </li>
-        <li class="nav-item">
+        <li class="nav-item ms-4">
           <nuxt-link class="nav-link text-dark" href="/personalisasi">
             <i class="bi bi-person fs-2"></i> <br>
             <!-- <span class="smallest">Personal</span> -->
