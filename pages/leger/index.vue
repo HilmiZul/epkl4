0fd4e7b808074756fde55522dbd92f8bb14da886
hilmizul <netspytux@gmail.com>
2025-11-17 17:51:14 +0700

    more intuitive UI

--- pages/leger/index.vue
+++ pages/leger/index.vue
@@ -3,12 +3,12 @@
     <div class="card-header">
       <span class="h4 quicksand fw-bold"><i class="bi bi-patch-check"></i> Leger</span>
     </div>
-    <div class="card-body small">
+    <div class="card-body">
       <loading-placeholder v-if="isLoading" row="1" col="12" />
       <div v-else-if="nilaiNotValid?.length > 0" class="alert alert-warning fs-6">
         Ada <span class="fw-bold">{{ nilaiNotValid.length }}</span> Nilai yang belum divalidasi
       </div>
-      <div class="row">
+      <div v-if="nilai?.length > 0" class="row">
         <div class="col-lg-6">
           <form @submit.prevent="getNilai">
             <div class="my-3 mt-0 input-group">
@@ -25,26 +25,48 @@
       </div>
       <div class="row">
         <div class="col-lg-12">
-          <div class="table-responsive">
+          <div v-if="isLoading">
+            <loading-placeholder row="1" col="12" />
+            <loading-placeholder row="1" col="12" />
+            <loading-placeholder row="1" col="12" />
+            <loading-placeholder row="1" col="12" />
+            <loading-placeholder row="1" col="12" />
+          </div>
+          <div v-else-if="nilai?.length < 1" class="text-center text-muted my-3">
+            <div class="fs-1"><i class="bi bi-database-fill"></i></div>
+            <div class="fs-4">Belum ada nilai</div>
+            <div class="pb-3">Nilai akan tampil apabila masa PKL selesai & peserta menyerehkan nilai.</div>
+          </div>
+          <ul v-else v-for="(n, i) in nilai" :key="i" class="list-group list-group-flush">
+            <nuxt-link :to="`/leger/${n.id}`" class="fw-bold">
+              <li class="list-group-item py-3 border-bottom border-2 border-grey">
+                {{ n.expand.siswa.nama }} <br>
+                <div class="text-muted mt-2">{{ n.expand.siswa.kelas }}</div>
+                <div v-if="n.isEntrust" class="text-muted mt-1">
+                  <i class="bi bi-person-vcard-fill"></i> Titip Sertifikat
+                </div>
+                <div class="text-muted mt-1">
+                  <span v-if="n.isValid"><i class="bi bi-check-circle-fill"></i> Valid</span>
+                  <span v-else class="text-danger"><i class="bi bi-dash-circle-fill"></i> Belum divalidasi</span>
+                </div>
+              <!-- <nuxt-link v-if="!n.isValid" :to="`/leger/${n.id}`" class="mt-2 btn btn-danger btn-sm border border-2 border-dark">Validasi sekarang <i class="bi bi-arrow-right"></i></nuxt-link> -->
+              </li>
+            </nuxt-link>
+          </ul>
+          <!-- <div class="table-responsive">
             <table class="table">
               <tbody>
-                <tr v-if="isLoading">
+                <tr >
                   <td colspan="5">
-                    <loading-placeholder row="1" col="12" />
-                    <loading-placeholder row="1" col="12" />
-                    <loading-placeholder row="1" col="12" />
-                    <loading-placeholder row="1" col="12" />
-                    <loading-placeholder row="1" col="12" />
+
                   </td>
                 </tr>
-                <tr v-else-if="nilai?.length < 1">
+                <tr >
                   <td colspan="5" class="text-center text-muted">
-                    <div class="fs-1"><i class="bi bi-database-fill"></i></div>
-                    <div class="fs-4">Belum ada nilai</div>
-                    <div class="pb-3">Nilai akan tampil apabila masa PKL selesai & peserta menyerehkan nilai.</div>
+
                   </td>
                 </tr>
-                <tr v-else v-for="(n, i) in nilai" :key="i" class="border-bottom border-2 border-dark">
+                <tr  class="border-bottom border-2 border-dark">
                   <td width="3%">
                     <span v-if="n.isValid" class="badge text-dark">{{ i+1 }}</span>
                     <nuxt-link v-else :to="`/leger/${n.id}`"><span class="badge bg-danger">{{ i+1 }}</span></nuxt-link>
@@ -65,7 +87,7 @@
                 </tr>
               </tbody>
             </table>
-          </div>
+          </div> -->
         </div>
       </div>
     </div>
