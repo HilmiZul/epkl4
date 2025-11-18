31e4adffa29ec11899533e51c69032aca6e0a65a
hilmizul <netspytux@gmail.com>
2025-11-18 14:51:37 +0700

    swap kolom dan mengubah elemen dengan opsi

--- pages/elemen/index.vue
+++ pages/elemen/index.vue
@@ -6,7 +6,7 @@
         <nuxt-link to="/elemen/tambah" class="btn btn-info btn-sm border border-2 border-dark"><i class="bi bi-plus-lg"></i> Tambah</nuxt-link>
       </span>
     </div>
-    <div class="card-body small">
+    <div class="card-body">
       <div class="row">
         <div class="col-lg-6">
           <div class="my-3 mt-0">
@@ -15,21 +15,29 @@
         </div>
         <div class="col align-content-center">
           <LoadingPlaceholder v-if="isLoading" col="12" row="1" />
-          <div v-else class="mb-3 text-grey float-end badge">{{ elements.length }} elemen</div>
+          <div v-else class="mb-3 text-grey float-end badge">{{ elements.length }} items</div>
         </div>
       </div>
       <div class="table-responsive">
         <table class="table table-hover table-striped table-borderless">
           <thead>
             <tr>
-              <th width="30%">Elemen</th>
-              <th width="40%">Capaian Pembelajaran</th>
               <th width="30%">Tujuan</th>
+              <th width="40%">Capaian Pembelajaran</th>
+              <th width="30%">Elemen</th>
+              <th v-if="role == 'admin' || role == 'jurusan'" width="5%">Hapus</th>
             </tr>
           </thead>
           <tbody>
             <tr v-if="isLoading" class="text-center my-5">
-              <td colspan="3">
+              <td v-if="role == 'admin' || role == 'jurusan'" colspan="4">
+                <LoadingPlaceholder col="12" row="1" />
+                <LoadingPlaceholder col="12" row="1" />
+                <LoadingPlaceholder col="12" row="1" />
+                <LoadingPlaceholder col="12" row="1" />
+                <LoadingPlaceholder col="12" row="1" />
+              </td>
+              <td v-else colspan="3">
                 <LoadingPlaceholder col="12" row="1" />
                 <LoadingPlaceholder col="12" row="1" />
                 <LoadingPlaceholder col="12" row="1" />
@@ -45,14 +53,38 @@
             </tr>
             <tr v-else v-for="e in elementsFiltered" :key="e.id">
               <td class="fw-bold">
-                <nuxt-link v-if="role == 'admin' || role == 'jurusan'" :to="`/elemen/${e.id}`" class="link">{{ e.elemen }}</nuxt-link>
-                <span v-else>{{ e.elemen }}</span>
+                <nuxt-link v-if="role == 'admin' || role == 'jurusan'" :to="`/elemen/${e.id}`" class="link"><span class="pre-text">{{ e.tujuan }}</span></nuxt-link>
+                <span v-else>{{ e.tujuan }}</span>
               </td>
               <td>{{ e.cp }}</td>
-              <td><span class="pre-text">{{ e.tujuan }}</span></td>
+              <td>{{ e.elemen }}</td>
+              <td v-if="role == 'admin' || role == 'jurusan'" class="smallest">
+                <button @click="setModalDeleteById(e.id, e.tujuan)" class="btn btn-danger btn-sm border border-2 border-dark" data-bs-toggle="modal" data-bs-target="#delete"><i class="bi bi-trash3"></i></button>
+              </td>
             </tr>
           </tbody>
         </table>
+        <!-- Single Modal: hapus iduka -->
+        <div class="modal" id="delete" aria-hidden="true" tabindex="-1">
+          <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content rounded-0 border border-3 border-dark shadow-lg">
+              <div class="modal-header rounded-0 h4 bg-danger fw-bold border-bottom border-3 border-dark">
+                Peringatan!
+              </div>
+              <div class="modal-body">
+                Yakin mau hapus Tujuan: <strong>{{ tujuan }}</strong> dari daftar elemen?
+              </div>
+              <div class="modal-footer border-0 justify-content-center">
+                <button v-if="!isDeleted" class="btn btn-danger border border-2 border-dark me-2" data-bs-dismiss="modal" @click="hapusData(elemen_id)" :disabled="isSending">
+                  <span v-if="isSending">Sedang menghapus</span>
+                  <span v-else>Hapus</span>
+                </button>
+                <button @click="() => { isDeleted = false; isSending = flase }" class="btn btn-light border border-2 border-dark" data-bs-dismiss="modal">Gajadi</button>
+              </div>
+            </div>
+          </div>
+        </div>
+
       </div>
     </div>
   </div>
@@ -68,10 +100,14 @@ let prokel = user?.user.value.program_keahlian
 let elements = ref([])
 let isLoading = ref(true)
 let keyword = ref('')
+let elemen_id = ref('')
+let tujuan = ref('')
+let isSending = ref(false)
+let isDeleted = ref(false)
 if(role == 'admin' && role == 'jurusan' && role == 'guru') navigateTo('/404')
 
-async function getElemenCp() {
-  isLoading.value = true
+async function getElemenCp(loading=true) {
+  isLoading.value = loading
   client.autoCancellation(false)
   let res = await client.collection('elemen_cp').getFullList({
     filter: "program_keahlian='"+prokel+"'",
@@ -92,7 +128,23 @@ const elementsFiltered = computed(() => {
   })
 })
 
-onMounted(() => getElemenCp())
+async function setModalDeleteById(id, name) {
+  elemen_id.value = id
+  tujuan.value = name
+}
+
+async function hapusData(id) {
+  client.autoCancellation(false)
+  await client.collection('elemen_cp').delete(id)
+}
+
+onMounted(() => {
+  getElemenCp()
+  client.autoCancellation(false)
+  client.collection('elemen_cp').subscribe('*', function(e) {
+    if(e.action == 'delete') getElemenCp(false)
+  },{})
+})
 </script>
 
 <style scoped>
