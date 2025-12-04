9ffbc6f09b86f60051685a632539c46bee5764a6
hilmizul <netspytux@gmail.com>
2025-12-05 04:48:50 +0700

    bug fix pagination IDUKA when search by keyword

--- pages/iduka/index.vue
+++ pages/iduka/index.vue
@@ -321,7 +321,7 @@ async function pagination(page, loading=true) {
     } else if(keyword.value != '') {
       searchActivated.value = true
       filterQuery = `isArchive=false && `
-      searchFilter = `iduka.nama~"${keyword.value}" || siswa.nama~"${keyword.value}"`
+      searchFilter = `nama~"${keyword.value}" || pembimbing_sekolah.nama~"${keyword.value}"`
     } else if(selectedProkel.value != '') {
       filterQuery = `program_keahlian="${selectedProkel.value}" && isArchive=false`
     } else {
