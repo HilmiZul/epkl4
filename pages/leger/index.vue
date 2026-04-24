773c78990d1643f8d77a16d425989a78ea26b4ad
hilmizul <netspytux@gmail.com>
2026-04-24 21:12:42 +0700

    new feat.: wakasek can export leger class by class to .xlsx file

--- pages/leger/index.vue
+++ pages/leger/index.vue
@@ -3,10 +3,92 @@
     <div class="card-header">
       <span class="h4 quicksand fw-bold"><i class="bi bi-patch-check"></i> Leger</span>
     </div>
-    <div class="card-body">
+
+    <div v-if="role == 'wakasek'" class="card-body">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="alert alert-info small px-4">
+            <li>Pilih kelas untuk menampilkan pratinjau nilai</li>
+            <li>Daftar yang muncul hanya yang sudah divalidasi oleh Guru pembimbing</li>
+            <li>Apabila semua nilai telah divalidasi, maka dapat diunduh kedalam berkas <code>.xlsx</code></li>
+          </div>
+        </div>
+        <div class="col-md-2">
+          <select @change="getNilaiFilterByClass" v-model="opsiKelas" name="opsiKelas" class="form form-select">
+            <option value="">Kelas</option>
+            <option v-for="(k,i) in kelas" :key="i" :value="k">{{ k }}</option>
+          </select>
+        </div>
+
+        <div v-if="listLeger.length > 0 && countSiswaOnRombel == listLeger.length" class="col-md-3">
+          <button @click="unduh()" class="btn btn-success border border-2 border-dark">
+            <i class="bi bi-download"></i> Unduh
+          </button>
+        </div>
+      </div>
+
+      <div class="row mt-3">
+        <div class="col-md-12">
+          <div class="mb-2 small">Total {{ listLeger.length }} dari {{ countSiswaOnRombel }}</div>
+
+          <div v-if="isLoading">
+            <LoadingPlaceholder row="1" col="12" />
+            <LoadingPlaceholder row="1" col="12" />
+            <LoadingPlaceholder row="1" col="12" />
+            <LoadingPlaceholder row="1" col="12" />
+            <LoadingPlaceholder row="1" col="12" />
+          </div>
+          <div v-else-if="!isLoading && listLeger.length < 1" class="text-center text-muted my-3">
+            <div class="fs-1"><i class="bi bi-database-fill"></i></div>
+            <div class="fs-4">Belum tersedia</div>
+            <div>Pilih kelas terlebih dahulu</div>
+          </div>
+          <div v-else class="table-responseive smallest">
+            <table class="table">
+              <thead>
+                <tr>
+                  <th width="3%">#</th>
+                  <th width="7%">NIS</th>
+                  <th>NAMA</th>
+                  <th>KELAS</th>
+                  <th width="10%">NILAI 1</th>
+                  <th width="10%">NILAI 1</th>
+                  <th width="10%">NILAI 1</th>
+                  <th width="10%">NILAI 1</th>
+                </tr>
+              </thead>
+
+              <tbody>
+                <tr v-if="!isLoading && listLeger.length < 1">
+                  <td colspan="7" class="text-center text-muted">
+                    <div class="fs-1"><i class="bi bi-database-fill"></i></div>
+                    <div class="fs-4">Belum tersedia</div>
+                  </td>
+                </tr>
+
+                <tr v-else v-for="(list,i) in listLeger" :key="list.id">
+                  <td>{{ i+1 }}</td>
+                  <td>{{ list?.expand.siswa.nis }}</td>
+                  <td>{{ list?.expand.siswa.nama }}</td>
+                  <td>{{ list?.expand.siswa.kelas }}</td>
+                  <td>{{ list.nilai_elemen1 }}</td>
+                  <td>{{ list.nilai_elemen2 }}</td>
+                  <td>{{ list.nilai_elemen3 }}</td>
+                  <td>{{ list.nilai_elemen4 }}</td>
+                </tr>
+              </tbody>
+            </table>
+
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div v-else-if="role == 'jurusan' || role == 'guru'" class="card-body">
       <div v-if="nilaiNotValid?.length > 0" class="alert alert-warning fs-6">
         Ada <span class="fw-bold">{{ nilaiNotValid.length }}</span> Nilai yang belum divalidasi
       </div>
+
       <div class="row">
         <div class="col-lg-6">
           <form @submit.prevent="getNilai">
@@ -22,6 +104,7 @@
           <div v-if="count_entrust" class="mb-3 text-white badge bg-dark">{{ count_entrust.length }} menitip sertifikat</div>
         </div>
       </div>
+
       <div class="row">
         <div class="col-lg-12">
           <div v-if="isLoading">
@@ -58,6 +141,7 @@
               </li>
             </nuxt-link>
           </ul>
+
           <!-- <div class="table-responsive">
             <table class="table">
               <tbody>
@@ -100,6 +184,8 @@
 </template>
 
 <script setup>
+import { utils, writeFileXLSX } from 'xlsx'
+
 definePageMeta({ middleware: 'auth' })
 useHead({ title: "Leger Nilai — e-PKL / SMKN 4 Tasikmalaya." })
 let client = usePocketBaseClient()
@@ -114,7 +200,27 @@ let keyword = ref('')
 let nilaiNotValid = ref('')
 let count_entrust = ref('')
 let searchActived = ref(false)
-if(role != 'jurusan' && role != 'admin' && role != 'guru') navigateTo('/404')
+if(role != role == 'tu') navigateTo('/404')
+let kelas = ref([
+  'TSM-1',
+  'TSM-2',
+  'TSM-3',
+  'TSM-4',
+  'TJKT-1',
+  'TJKT-2',
+  'TJKT-3',
+  'TJKT-4',
+  'PPLG-1',
+  'PPLG-2',
+  'PPLG-3',
+  'PPLG-4',
+  'DKV-1',
+  'DKV-2',
+  'TOI',
+])
+let opsiKelas = ref('')
+let listLeger = ref([])
+let countSiswaOnRombel = ref(0)
 
 async function getNilai(loading=true) {
   isLoading.value = loading
@@ -159,6 +265,64 @@ async function getEntrust() {
     count_entrust.value = res
     isLoading.value = false
   }
+}
+
+async function getNilaiFilterByClass() {
+  isLoading.value = true
+
+  let res_nilai = await client.collection('nilai').getFullList({
+    filter: `siswa.kelas="${opsiKelas.value}" && isValid=true`,
+    expand: `iduka, siswa, program_keahlian`,
+    sort: `program_keahlian, siswa.kelas, siswa.nama`
+  })
+
+  let res_rombel = await client.collection('siswa').getList(1,1, {
+    filter: `kelas="${opsiKelas.value}"`
+  })
+
+  if(res_rombel) {
+    countSiswaOnRombel.value = res_rombel.totalItems
+    if(res_nilai) {
+      listLeger.value = res_nilai
+      isLoading.value = false
+    }
+  }
+}
+
+function unduh() {
+  const raw_data = listLeger.value
+
+  // flatten objects
+  const rows = raw_data.map((row,i) => ({
+    no: i+1,
+    nis: row.expand.siswa.nis,
+    nisn: row.expand.siswa.nisn,
+    nama: row.expand.siswa.nama,
+    kelas: `XII.${row.expand.siswa.kelas}`,
+    iduka: row.expand.iduka.nama,
+    instruktur: row.expand.iduka.pembimbing_iduka,
+    nilai_elemen1: row.nilai_elemen1,
+    nilai_elemen2: row.nilai_elemen2,
+    nilai_elemen3: row.nilai_elemen3,
+    nilai_elemen4: row.nilai_elemen4,
+    sakit: row.sakit,
+    izin: row.izin,
+    absen: row.tanpa_keterangan,
+  }));
+
+  // generate worksheet & workbook
+  const worksheet = utils.json_to_sheet(rows);
+  const workbook = utils.book_new();
+
+  utils.book_append_sheet(workbook, worksheet, `XII.${opsiKelas.value}`);
+
+  // fix headers
+  utils.sheet_add_aoa(worksheet, [["NO", "NIS", "NISN", "NAMA", "KELAS", 
+                                    "IDUKA", "INSTRUKTUR", "NILAI 1", "NILAI 2", "NILAI 3", "NILAI 4",
+                                    "SAKIT", "IZIN", "ABSEN"]], { origin: "A1" });
+  
+  // buat file dan download dengan ext .xlsx
+  writeFileXLSX(workbook, `Leger Rapor PKL - XII.${opsiKelas.value}.xlsx`, { compression: true });
 }
 
 onMounted(() => {
