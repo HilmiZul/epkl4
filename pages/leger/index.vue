10752a0595713ab965020540aa993378263a2e05
hilmizul <netspytux@gmail.com>
2026-06-30 16:23:39 +0700

    check if all journal are validated, show leger items, otherwise lock this page

--- pages/leger/index.vue
+++ pages/leger/index.vue
@@ -3,7 +3,7 @@
     <div class="card-header">
       <span class="h4 quicksand fw-bold"><i class="bi bi-patch-check"></i> Leger</span>
       <span v-if="role == 'jurusan' || role == 'guru'" class="float-end">
-        <NuxtLink to="/leger/tambah" class="btn btn-info btn-sm border border-2 border-dark">
+        <NuxtLink v-if="journalValidCount < 1" to="/leger/tambah" class="btn btn-info btn-sm border border-2 border-dark">
           <i class="bi bi-plus"></i> Tambah
         </NuxtLink>
       </span>
@@ -93,100 +93,116 @@
     </div>
 
     <div v-else-if="role == 'jurusan' || role == 'guru'" class="card-body">
-      <div v-if="nilaiNotValid?.length > 0" class="alert alert-warning fs-6">
-        Ada <span class="fw-bold">{{ nilaiNotValid.length }}</span> Nilai yang belum divalidasi
-      </div>
+      <div v-if="journalValidCount < 1">
+        <div v-if="nilaiNotValid?.length > 0" class="alert alert-warning fs-6">
+          Ada <span class="fw-bold">{{ nilaiNotValid.length }}</span> Nilai yang belum divalidasi
+        </div>
 
-      <div class="row">
-        <div class="col-lg-6">
-          <form @submit.prevent="getNilai">
-            <div class="my-3 mt-0 input-group">
-              <input v-model="keyword" type="search" class="form form-control form-control-lg" placeholder="🔎 Cari nama peserta" />
-              <button class="btn btn-info ms-2 border border-2 border-dark">Cari</button>
+        <div class="row">
+          <div class="col-lg-6">
+            <form @submit.prevent="getNilai">
+              <div class="my-3 mt-0 input-group">
+                <input v-model="keyword" type="search" class="form form-control form-control-lg" placeholder="🔎 Cari nama peserta" />
+                <button class="btn btn-info ms-2 border border-2 border-dark">Cari</button>
+              </div>
+            </form>
+          </div>
+          <div class="col-lg-6 align-content-center text-end">
+            <LoadingPlaceholder v-if="isLoading" col="12" row="1" />
+            <div v-if="nilai" class="mb-3 text-dark badge me-2">{{ nilai.length }} peserta</div>
+            <div v-if="count_entrust" class="mb-3 text-white badge bg-dark">{{ count_entrust.length }} menitip sertifikat</div>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-lg-12">
+            <div v-if="isLoading">
+              <loading-placeholder row="1" col="12" />
+              <loading-placeholder row="1" col="12" />
+              <loading-placeholder row="1" col="12" />
+              <loading-placeholder row="1" col="12" />
+              <loading-placeholder row="1" col="12" />
             </div>
-          </form>
+            <div v-else-if="nilai?.length < 1" class="text-center text-muted my-3">
+              <span v-if="searchActived">
+                <div class="fs-1"><i class="bi bi-search"></i></div>
+                <div class="fs-4">Pencarian tidak ditemukan</div>
+              </span>
+              <span v-else>
+                <div class="fs-1"><i class="bi bi-database-fill"></i></div>
+                <div class="fs-4">Belum tersedia</div>
+                <div class="pb-3">Nilai akan tampil apabila masa PKL selesai & peserta menyerehkan nilai.</div>
+              </span>
+            </div>
+            <ul v-else v-for="(n, i) in nilai" :key="i" class="list-group list-group-flush">
+              <nuxt-link :to="`/leger/${n.id}`" class="fw-bold">
+                <li class="list-group-item py-3 border-bottom border-2 border-grey">
+                  {{ n.expand.siswa.nama }} <br>
+                  <div class="text-muted mt-2">{{ n.expand.siswa.kelas }}</div>
+                  <div v-if="n.isEntrust" class="text-muted mt-1">
+                    <i class="bi bi-person-vcard-fill"></i> Titip Sertifikat
+                  </div>
+                  <div class="text-muted mt-1">
+                    <span v-if="n.isValid"><i class="bi bi-check-circle-fill"></i> Valid</span>
+                    <span v-else class="text-danger"><i class="bi bi-dash-circle-fill"></i> Belum divalidasi</span>
+                  </div>
+                <!-- <nuxt-link v-if="!n.isValid" :to="`/leger/${n.id}`" class="mt-2 btn btn-danger btn-sm border border-2 border-dark">Validasi sekarang <i class="bi bi-arrow-right"></i></nuxt-link> -->
+                </li>
+              </nuxt-link>
+            </ul>
+
+            <!-- <div class="table-responsive">
+              <table class="table">
+                <tbody>
+                  <tr >
+                    <td colspan="5">
+
+                    </td>
+                  </tr>
+                  <tr >
+                    <td colspan="5" class="text-center text-muted">
+
+                    </td>
+                  </tr>
+                  <tr  class="border-bottom border-2 border-dark">
+                    <td width="3%">
+                      <span v-if="n.isValid" class="badge text-dark">{{ i+1 }}</span>
+                      <nuxt-link v-else :to="`/leger/${n.id}`"><span class="badge bg-danger">{{ i+1 }}</span></nuxt-link>
+                    </td>
+                    <td>
+                      <nuxt-link :to="`/leger/${n.id}`" class="link fw-bold">{{ n.expand.siswa.nama }}</nuxt-link> <br>
+                      <div class="text-muted fw-bold small mt-2">{{ n.expand.siswa.kelas }}</div>
+                      <div v-if="n.isEntrust" class="text-muted small">
+                        <i class="bi bi-person-vcard-fill"></i> Titip Sertifikat
+                      </div>
+                      <div class="text-muted small">
+                        <span v-if="n.isValid"><i class="bi bi-check-circle-fill"></i> Valid</span>
+                        <span v-else class="text-danger"><i class="bi bi-dash-circle-fill"></i> Belum divalidasi</span>
+                      </div>
+                      <nuxt-link v-if="n.isValid" :to="`/leger/${n.id}`" class="mt-2 btn btn-info btn-sm border border-2 border-dark">Lihat <i class="bi bi-arrow-right"></i></nuxt-link>
+                      <nuxt-link v-else :to="`/leger/${n.id}`" class="mt-2 btn btn-danger btn-sm border border-2 border-dark">Validasi sekarang <i class="bi bi-arrow-right"></i></nuxt-link>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div> -->
+          </div>
         </div>
-        <div class="col-lg-6 align-content-center text-end">
-          <LoadingPlaceholder v-if="isLoading" col="12" row="1" />
-          <div v-if="nilai" class="mb-3 text-dark badge me-2">{{ nilai.length }} peserta</div>
-          <div v-if="count_entrust" class="mb-3 text-white badge bg-dark">{{ count_entrust.length }} menitip sertifikat</div>
+        <!-- tutup kontent jurusan / guru pemmbimbing -->
+
+      </div>
+      <!-- tutup cek apabila jurnal belum validasi semua -->
+
+      <div v-else class="p-4 text-muted">
+        <div class="text-center fs-4">
+          <i class="bi bi-lock-fill fs-1"></i> <br>
+          Leger Nilai Terkunci
+        </div>
+        <div class="text-center">
+          Pastikan semua <NuxtLink to="/jurnal" class="text-muted">Jurnal</NuxtLink> sudah divalidasi. Mari apresiasi pekerjaan yang sudah dilakukan Peserta didik.
         </div>
       </div>
 
-      <div class="row">
-        <div class="col-lg-12">
-          <div v-if="isLoading">
-            <loading-placeholder row="1" col="12" />
-            <loading-placeholder row="1" col="12" />
-            <loading-placeholder row="1" col="12" />
-            <loading-placeholder row="1" col="12" />
-            <loading-placeholder row="1" col="12" />
-          </div>
-          <div v-else-if="nilai?.length < 1" class="text-center text-muted my-3">
-            <span v-if="searchActived">
-              <div class="fs-1"><i class="bi bi-search"></i></div>
-              <div class="fs-4">Pencarian tidak ditemukan</div>
-            </span>
-            <span v-else>
-              <div class="fs-1"><i class="bi bi-database-fill"></i></div>
-              <div class="fs-4">Belum tersedia</div>
-              <div class="pb-3">Nilai akan tampil apabila masa PKL selesai & peserta menyerehkan nilai.</div>
-            </span>
-          </div>
-          <ul v-else v-for="(n, i) in nilai" :key="i" class="list-group list-group-flush">
-            <nuxt-link :to="`/leger/${n.id}`" class="fw-bold">
-              <li class="list-group-item py-3 border-bottom border-2 border-grey">
-                {{ n.expand.siswa.nama }} <br>
-                <div class="text-muted mt-2">{{ n.expand.siswa.kelas }}</div>
-                <div v-if="n.isEntrust" class="text-muted mt-1">
-                  <i class="bi bi-person-vcard-fill"></i> Titip Sertifikat
-                </div>
-                <div class="text-muted mt-1">
-                  <span v-if="n.isValid"><i class="bi bi-check-circle-fill"></i> Valid</span>
-                  <span v-else class="text-danger"><i class="bi bi-dash-circle-fill"></i> Belum divalidasi</span>
-                </div>
-              <!-- <nuxt-link v-if="!n.isValid" :to="`/leger/${n.id}`" class="mt-2 btn btn-danger btn-sm border border-2 border-dark">Validasi sekarang <i class="bi bi-arrow-right"></i></nuxt-link> -->
-              </li>
-            </nuxt-link>
-          </ul>
-
-          <!-- <div class="table-responsive">
-            <table class="table">
-              <tbody>
-                <tr >
-                  <td colspan="5">
-
-                  </td>
-                </tr>
-                <tr >
-                  <td colspan="5" class="text-center text-muted">
-
-                  </td>
-                </tr>
-                <tr  class="border-bottom border-2 border-dark">
-                  <td width="3%">
-                    <span v-if="n.isValid" class="badge text-dark">{{ i+1 }}</span>
-                    <nuxt-link v-else :to="`/leger/${n.id}`"><span class="badge bg-danger">{{ i+1 }}</span></nuxt-link>
-                  </td>
-                  <td>
-                    <nuxt-link :to="`/leger/${n.id}`" class="link fw-bold">{{ n.expand.siswa.nama }}</nuxt-link> <br>
-                    <div class="text-muted fw-bold small mt-2">{{ n.expand.siswa.kelas }}</div>
-                    <div v-if="n.isEntrust" class="text-muted small">
-                      <i class="bi bi-person-vcard-fill"></i> Titip Sertifikat
-                    </div>
-                    <div class="text-muted small">
-                      <span v-if="n.isValid"><i class="bi bi-check-circle-fill"></i> Valid</span>
-                      <span v-else class="text-danger"><i class="bi bi-dash-circle-fill"></i> Belum divalidasi</span>
-                    </div>
-                    <nuxt-link v-if="n.isValid" :to="`/leger/${n.id}`" class="mt-2 btn btn-info btn-sm border border-2 border-dark">Lihat <i class="bi bi-arrow-right"></i></nuxt-link>
-                    <nuxt-link v-else :to="`/leger/${n.id}`" class="mt-2 btn btn-danger btn-sm border border-2 border-dark">Validasi sekarang <i class="bi bi-arrow-right"></i></nuxt-link>
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-          </div> -->
-        </div>
-      </div>
     </div>
   </div>
 </template>
@@ -230,6 +246,8 @@ let opsiKelas = ref('')
 let listLeger = ref([])
 let countSiswaOnRombel = ref(0)
 let walikelas = ref('')
+
+let journalValidCount = ref(0)
 
 async function getNilai(loading=true) {
   isLoading.value = loading
@@ -338,10 +356,23 @@ function unduhLeger() {
   writeFileXLSX(workbook, `Leger Rapor PKL - XII.${opsiKelas.value}.xlsx`, { compression: true });
 }
 
+// FETCH JURNAL by pembimbing.id where draft=false and isValid=false
+async function getJurnalByPembimbing() {
+  let res = await client.collection('jurnal').getList(1, 1, {
+    filter: `iduka.pembimbing_sekolah="${user?.user.value.id}" && isDraft=false && isValid=false`
+  })
+
+  if(res) {
+    journalValidCount.value = res.totalItems
+  }
+}
+
+
 onMounted(() => {
   getNilai()
   getEntrust()
   getNilaiByNotVerify()
+  getJurnalByPembimbing()
   // client.autoCancellation(false)
   // client.collection('nilai').subscribe('*', function(e) {
   //   if(e.action == 'create' || e.action == 'update') getNilai(false)
