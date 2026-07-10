774dd8595b81a71bfc24a309fa1a46186bef14cf
hilmizul <netspytux@gmail.com>
2026-07-10 22:30:02 +0700

    add new form and guru pembimbing allowed to edit

--- pages/personalisasi/index.vue
+++ pages/personalisasi/index.vue
@@ -3,6 +3,7 @@
     <div class="card-header">
       <span class="h4 quicksand fw-bold"><i class="bi bi-person-fill"></i> Personalisasi</span>
     </div>
+
     <div class="card-body">
       <div class="row">
         <div class="col-md-6">
@@ -12,10 +13,38 @@
               <label for="nama">Nama Lengkap</label>
               <input v-model="form.nama" type="text" id="nama" class="form form-control form-control-lg" placeholder="nama lengkap dan gelar" required>
             </div>
+
             <div class="mb-4">
               <label for="nip">NIP</label>
               <input v-model="form.nip" :disabled="form.nama.length < 4" type="text" id="nip" class="form form-control form-control-lg" placeholder="kosongkan jika tidak ada">
             </div>
+
+            <div class="my-4">
+              <label for="pangkat_golongan">Pangkat Golongan</label>
+              <select v-model="form.pangkat_golongan" :disabled="isLoading" id="pangkat_golongan" class="form form-control form-select form-select-lg" required>
+                <option disabled value="">—</option>
+                <option value="III/a">III/a</option>
+                <option value="III/b">III/b</option>
+                <option value="III/c">III/c</option>
+                <option value="III/d">III/d</option>
+                <option value="IV/a">IV/a</option>
+                <option value="IV/b">IV/b</option>
+                <option value="IV/c">IV/c</option>
+                <option value="IV/d">IV/d</option>
+                <option value="IV/e">IV/e</option>
+                <option value="IX/IX">IX/IX</option>
+              </select>
+            </div>
+
+            <div class="my-4">
+              <label for="kelompok_mapel">Kelompok Mapel</label>
+              <select v-model="form.kelompok_mapel" :disabled="isLoading" id="kelompok_mapel" class="form form-control form-select form-select-lg" required>
+                <option disabled value="">—</option>
+                <option value="Guru Kejuruan">Guru Kejuruan</option>
+                <option value="Guru Umum">Guru Umum</option>
+              </select>
+            </div>
+
             <button :disabled="isSending || form.nama.length < 4" class="btn btn-success me-2 border border-2 border-dark">
               <span v-if="!isSending">Simpan</span>
               <span v-else>Sedang menyimpan</span>
@@ -25,6 +54,8 @@
         </div>
       </div>
     </div>
+    <!-- tutup .card-body -->
+
   </div>
 </template>
 
@@ -38,10 +69,14 @@ let isSaved = ref(false)
 let isSending = ref(false)
 let isLoading = ref(true)
 let isError = ref(false)
+
 let form = ref({
   nama: user?.user.value.nama,
   nip: user?.user.value.nip,
+  pangkat_golongan: '',
+  kelompok_mapel: '',
 })
+
 if(role != 'jurusan' && role != 'admin' && role != 'guru') navigateTo('/404')
 
 async function updatePersonal() {
@@ -61,4 +96,24 @@ async function updatePersonal() {
     isErrorOldPass.value = true
   }
 }
+
+
+async function getCurrentTeacherByCurrentLoginId() {
+  isLoading.value = true
+
+  let data = await client.collection('teacher_users').getOne(user?.user.value.id, {
+    expand: `program_keahlian`
+  })
+
+  if(data) {
+    form.value = data
+
+    isLoading.value = false
+  }
+}
+
+
+onMounted(() => {
+  getCurrentTeacherByCurrentLoginId()
+})
 </script>
